e40cfa600524e805d556f58351a21937bb5bf81a
Blauto <Blauto@DESKTOP-AJEVJGM>
2018-07-26 09:57:16 +0200

    Preload css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 
 class App extends Component {
 
+  state = {
+     footTable: [
+       { name: 'awokado', carbohydrates: 8.53, fat: 14.66, protein: 2},
+       { name: 'boczniak', carbohydrates: 5, fat: 0.2, protein: 2.5},
+       { name: 'borówka', carbohydrates: 14, fat: 0, protein: 1}
+     ]
+  }
+
   render() {
     return (
       <div className="App">
-        
+      
       </div>
     );
   }
